e619f81eccea28e5495af2ec691453c6aaa87692
24LP2437 24LP2437 <24LP2437@G304-CC0216.local>
2025-02-05 12:34:26 +0800

    nnnn

--- src/app/genre-section/page.tsx
+++ src/app/genre-section/page.tsx
@@ -1,4 +1,24 @@
 "use client";
 
-const GenrePage = () => {};
+import { Header } from "@/components/Header";
+import { getData } from "@/util/Data";
+import { Button } from "@/components/ui/button";
+
+const GenrePage = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <div>
+          <h1>Search filter</h1>
+          <div>
+            <h2>Genres</h2>
+            <h6>see lists of movies by genre</h6>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 export default GenrePage;
--- src/components/genre.tsx
+++ src/components/genre.tsx
@@ -15,10 +15,15 @@ import { Type } from "./GenreType";
 import { ChevronDown } from "lucide-react";
 import { GenreType } from "@/util/Movietype";
 import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
+type Genre = {
+  data: GenreType[];
+};
 
 export default async function Genre() {
   const data = await getData("/genre/movie/list?language=en");
-  console.log("genre", data);
+  console.log(data);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -34,15 +39,17 @@ export default async function Genre() {
           See lists of movies by genre
         </DropdownMenuItem>
         <DropdownMenuSeparator className="w-[33rem] m-auto border-[1.5px] " />
-        <DropdownMenuGroup className="flex flex-wrap gap-[16px]">
+        <DropdownMenuGroup className="flex flex-wrap gap-[16px] mt-4 ">
           {data.genres.map((genre: GenreType, index: number) => {
             return (
-              <Button
-                key={index}
-                className="h-[20px] p-0 pl-[10px] text-[12px] bg-secondary text-secondary-foreground"
-              >
-                {genre.name} <ChevronRight />
-              </Button>
+              <Link href={`/genre-section/?page=1&genreIds=${genre.id}`}>
+                <Button
+                  key={index}
+                  className="h-[20px] p-0 pl-[10px] text-[12px] bg-secondary text-secondary-foreground"
+                >
+                  {genre.name} <ChevronRight />
+                </Button>
+              </Link>
             );
           })}
         </DropdownMenuGroup>
